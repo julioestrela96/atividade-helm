bd55e24451a70dcb6d12baaa237a49a78a9f114d
Diego Pessoa <diego.pessoa@ifpb.edu.br>
2023-11-23 17:38:32 -0300

    adiciona conexão com o postgresql

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'todoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-todo',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
